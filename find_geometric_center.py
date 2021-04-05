fba4222bc718a62fc0ae295a5c974a3bd9fda4da
Werthis <lukaszstawecki@gmail.com>
2021-04-05 15:31:23 +0200

    Just need cleaning up #1

--- find_geometric_center.py
+++ find_geometric_center.py
@@ -11,8 +11,8 @@ class GeometricCenter():
 
     def check_number_of_dimensions(self):
         for i in self._list_of_points:
-            self.dimension = len(i)
-        return self.dimension
+            self._dimension = len(i)
+        return self._dimension
 
     def find_geometric_center(self):
         _summ_of_one_dimension_coordinates = 0
@@ -20,8 +20,8 @@ class GeometricCenter():
         if self._list_of_points != None:
             for i in range(len(self._list_of_points)):
                 self._list_of_all_coordinates += self._list_of_points[i]
-        for x in range(self.dimension):
-            for i in range(x, len(self._list_of_all_coordinates), self.dimension):
+        for x in range(self._dimension):
+            for i in range(x, len(self._list_of_all_coordinates), self._dimension):
                 _summ_of_one_dimension_coordinates += self._list_of_all_coordinates[i]
             _coordinate_of_geo_center = _summ_of_one_dimension_coordinates/(self._number_of_points)
             _summ_of_one_dimension_coordinates = 0
@@ -30,19 +30,17 @@ class GeometricCenter():
 
     def gui_communication(self, all_points, number_of_dimentions, number_of_points):
         self._list_of_all_coordinates = all_points
-        self.dimension = number_of_dimentions
+        self._dimension = number_of_dimentions
         self._number_of_points = number_of_points
         self._list_of_points = None
-        print(self._list_of_points)
-        if self.dimension == 0:
+        if self._dimension == 0:
             self.check_number_of_dimensions()
         message_to_gui = self.find_geometric_center()
-        self.geometric_center = []
+        # self.geometric_center.clear()
         return message_to_gui
 
     def __str__(self):
         return f'Geometric center of points: {self._list_of_points} is {self.geometric_center}'
-
 
 if __name__ == "__main__":
     list_of_points = [(1,1), (2, 2), (3, 1)]
--- gui_find_geometric_center.py
+++ gui_find_geometric_center.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-# from tkinter import *
 import find_geometric_center as find_geo
 
 class GuiGeometric():
@@ -7,13 +6,13 @@ class GuiGeometric():
     def __init__(self, backend):
         self._backend = backend
         self._numbers_from_entry = []    
-        self.coordinates_list = []
-        self.list_of_points = []
+        self._coordinates_list = []
+        self._list_of_points = []
 
         self.window = tk.Tk()
         self.window.title('Geometric Center Finder')
 
-        self.coordinates_info = tk.StringVar()
+        self.geometric_center_info = tk.StringVar()
         self.dimentions_number = tk.StringVar()
 
         info_label = tk.Label(self.window, text='Geometric Center Finder', font=('arial', 14))
@@ -46,38 +45,33 @@ class GuiGeometric():
             self._numbers_from_entry.append(self.number)
 
 
-        button_take_numbers = tk.Button(self.window, text='Add point', command=self.take_values)
+        button_take_numbers = tk.Button(self.window, text='Add point', font=('arial', 12), command=self.take_values)
         button_take_numbers.pack()
 
-        label_print = tk.Label(self.window, textvariable = self.coordinates_info, font=('arial', 12))
-        label_print.pack()
-
-        self.listbox_of_coordinates = tk.Listbox(self.window, height=5)
+        self.listbox_of_coordinates = tk.Listbox(self.window, height=5, font=('arial', 12))
         self.listbox_of_coordinates.pack()
 
         button_send_all_to_backend = tk.Button(self.window, text='Submit', command=self.backend_communication)
         button_send_all_to_backend.pack()
 
+        label_print_geometric_center = tk.Label(self.window, textvariable = self.geometric_center_info, font=('arial', 12))
+        label_print_geometric_center.pack()
 
     def take_values(self):
         for i in self._numbers_from_entry:
-            self.coordinates_list.append(int(i.get()))
+            self._coordinates_list.append(int(i.get()))
             i.set('')
-        self.list_of_points += self.coordinates_list
-        self.coordinates_info.set(f'new point is {self.coordinates_list}')
-        print(self.list_of_points)
-        self.listbox_of_coordinates.insert(0, self.coordinates_list)
-        self.coordinates_list.clear()
-        return self.list_of_points
+        self._list_of_points += self._coordinates_list
+        self.listbox_of_coordinates.insert(0, self._coordinates_list)
+        self._coordinates_list.clear()
+        return self._list_of_points
 
     def backend_communication(self):
-        print(self.list_of_points)
-        number_of_points = (len(self.list_of_points)/self.number_of_dimentions)
+        number_of_points = (len(self._list_of_points)/self.number_of_dimentions)
+        self.list_from_backend = self._backend.gui_communication(all_points= self._list_of_points, number_of_dimentions= self.number_of_dimentions, number_of_points= number_of_points)
+        self.geometric_center_info.set(f'Coordinates of geometric center:\n{self.list_from_backend}')
 
-        list_for_backend = self._backend.gui_communication(all_points = self.list_of_points, number_of_dimentions = self.number_of_dimentions, number_of_points= number_of_points)
-        print(list_for_backend)
-
-        return list_for_backend
+        return self.list_from_backend
 
     def start(self):
         self.window.mainloop()

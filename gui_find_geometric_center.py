a67d305efd932f4f403649e15c24f5db1ab6353a
Werthis <lukaszstawecki@gmail.com>
2021-04-18 16:06:19 +0200

    Done with tkinter #1

--- gui_find_geometric_center.py
+++ gui_find_geometric_center.py
@@ -15,10 +15,23 @@ class GuiGeometric():
         self.geometric_center_info = tk.StringVar()
         self.dimentions_number = tk.StringVar()
 
-        info_label = tk.Label(self.window, text='Geometric Center Finder', font=('arial', 14))
-        label_dimentions_question = tk.Label(self.window, text='Write number of dimentions', font=('arial', 12))
-        entry_number_of_dimentions = tk.Entry(self.window, textvariable=self.dimentions_number, font=('arial', 12))
-        button_confirm_dimentions = tk.Button(self.window, text='confirm', font=('arial', 12), command = self.make_new_widgets)
+        info_label = tk.Label(
+            self.window, 
+            text='Geometric Center Finder', 
+            font=('arial', 14))
+        label_dimentions_question = tk.Label(
+            self.window, 
+            text='Write number of dimentions', 
+            font=('arial', 12))
+        entry_number_of_dimentions = tk.Entry(
+            self.window, 
+            textvariable=self.dimentions_number, 
+            font=('arial', 12))
+        button_confirm_dimentions = tk.Button(
+            self.window, 
+            text='confirm', 
+            font=('arial', 12), 
+            command = self.make_new_widgets)
 
         info_label.pack()
         label_dimentions_question.pack()
@@ -31,30 +44,51 @@ class GuiGeometric():
 
     def make_new_widgets(self):
         self.get_number_of_dimentions()
-
-        self.put_two_numbers = tk.Label(self.window, text='put coordinates here:', font=('arial', 12))
+        self.put_two_numbers = tk.Label(
+            self.window, 
+            text='put coordinates here:', 
+            font=('arial', 12))
         self.put_two_numbers.pack()
 
         for i in range(1, self.number_of_dimentions + 1):
-            coordinates_label = tk.Label(self.window, text=f'Dimension {i}', font=('arial', 12))
+            coordinates_label = tk.Label(
+                self.window, 
+                text=f'Dimension {i}', 
+                font=('arial', 12))
             coordinates_label.pack()
 
             self.number = tk.StringVar()
-            coordination_entry = tk.Entry(self.window,  textvariable = self.number, font=('arial', 12))
+            coordination_entry = tk.Entry(
+                self.window, 
+                textvariable = self.number, 
+                font=('arial', 12))
             coordination_entry.pack()
             self._numbers_from_entry.append(self.number)
 
 
-        button_take_numbers = tk.Button(self.window, text='Add point', font=('arial', 12), command=self.take_values)
+        button_take_numbers = tk.Button(
+            self.window, 
+            text='Add point', 
+            font=('arial', 12), 
+            command=self.take_values)
         button_take_numbers.pack()
 
-        self.listbox_of_coordinates = tk.Listbox(self.window, height=5, font=('arial', 12))
+        self.listbox_of_coordinates = tk.Listbox(
+            self.window, 
+            height=5, 
+            font=('arial', 12))
         self.listbox_of_coordinates.pack()
 
-        button_send_all_to_backend = tk.Button(self.window, text='Submit', command=self.backend_communication)
+        button_send_all_to_backend = tk.Button(
+            self.window, 
+            text='Submit', 
+            command=self.backend_communication)
         button_send_all_to_backend.pack()
 
-        label_print_geometric_center = tk.Label(self.window, textvariable = self.geometric_center_info, font=('arial', 12))
+        label_print_geometric_center = tk.Label(
+            self.window, 
+            textvariable = self.geometric_center_info, 
+            font=('arial', 12))
         label_print_geometric_center.pack()
 
     def take_values(self):
@@ -70,7 +104,6 @@ class GuiGeometric():
         number_of_points = (len(self._list_of_points)/self.number_of_dimentions)
         self.list_from_backend = self._backend.gui_communication(all_points= self._list_of_points, number_of_dimentions= self.number_of_dimentions, number_of_points= number_of_points)
         self.geometric_center_info.set(f'Coordinates of geometric center:\n{self.list_from_backend}')
-
         return self.list_from_backend
 
     def start(self):
